02c6601e7656f5a3750d8ee0408c6b8c6e223520
Marco Simile <marco.simile@outlook.it>
2022-08-29 13:17:22 +0200

    feat: conditional_rendering-02 complete

--- react-exercises/src/Welcome.js
+++ react-exercises/src/Welcome.js
@@ -6,7 +6,7 @@ export class Welcome extends React.Component {
     return (
       <div>
         <p>Welcome, {this.props.name}!</p>
-        <Age age={18} />
+        {this.props.age && <Age age = {this.props.age}/>}
       </div>
     );
   }
