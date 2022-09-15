984b499ed7b7873b9a73ac049c48aaa6871f922e
Marco Simile <marco.simile@outlook.it>
2022-09-15 12:46:40 +0200

    function-components-01 complete

--- react-exercises/src/Welcome.js
+++ react-exercises/src/Welcome.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { Age } from "./Age";
 
-export class Welcome extends React.Component {
-  render() {
-    return (
-      <div className="welcome">
-        <p>Welcome, {this.props.name}!</p>
-        {this.props.name === "John" && <Age age={18} />}
-      </div>
-    );
-  }
+export function Welcome({ name = "visitor" }) {
+  return (
+    <div>
+      <p>Welcome, {name}!</p>
+      {name === "John" && <Age age={18} />}
+    </div>
+  );
 }
 
-Welcome.defaultProps = {
-  name: "visitor",
-  age: "not defined",
-};
+// export class Welcome extends React.Component {
+//   render() {
+//     return (
+//       <div className="welcome">
+//         <p>Welcome, {this.props.name}!</p>
+//         {this.props.name === "John" && <Age age={18} />}
+//       </div>
+//     );
+//   }
+// }
+
+// Welcome.defaultProps = {
+//   name: "visitor",
+//   age: "not defined",
+// };
