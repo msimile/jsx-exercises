a160de2784175fff8bcedc17baa5470ab2284737
Marco Simile <marco.simile@outlook.it>
2022-09-15 22:45:25 +0200

    useState-02 complete

--- react-exercises/src/ClickCounter.js
+++ react-exercises/src/ClickCounter.js
@@ -1,31 +1,46 @@
-import React from "react";
-import { CounterButton } from "./CounterButton";
+import React, {useState} from "react";
+// import { CounterButton } from "./CounterButton";
 
-export class ClickCounter extends React.Component {
-  state = {
-    count: this.props.initialValue,
-  };
+export function ClickCounter({initialValue = 0}) {
+  const [counter, setCounter] = useState(initialValue);
 
-  handleClickCounter = (event) => {
-    this.setState((state) => {
-      return {
-        count: state.count + this.props.incrementBy,
-      };
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>Click Counter: {this.state.count}</h1>
-        {/* <button onClick={this.handleClickCounter}>Increment</button> */}
-        <CounterButton counter={this.handleClickCounter} />
-      </div>
-    );
+  function handleClickCounter() {
+    setCounter((c) => c + 1);
   }
+
+  return (
+    <div>
+      <h2>Counter: {counter}</h2>
+      <button onClick={handleClickCounter}>Increment</button>
+    </div>
+  );
 }
 
-ClickCounter.defaultProps = {
-  initialValue: 0,
-  incrementBy: 1,
-};
+// export class ClickCounter extends React.Component {
+//   state = {
+//     count: this.props.initialValue,
+//   };
+
+//   handleClickCounter = (event) => {
+//     this.setState((state) => {
+//       return {
+//         count: state.count + this.props.incrementBy,
+//       };
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Click Counter: {this.state.count}</h1>
+//         {/* <button onClick={this.handleClickCounter}>Increment</button> */}
+//         <CounterButton counter={this.handleClickCounter} />
+//       </div>
+//     );
+//   }
+// }
+
+// ClickCounter.defaultProps = {
+//   initialValue: 0,
+//   incrementBy: 1,
+// };
