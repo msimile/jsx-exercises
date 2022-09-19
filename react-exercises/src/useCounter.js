dd1d1bb2d6c14ef8c47a5e67291082cd4a1071e9
Marco Simile <marco.simile@outlook.it>
2022-09-19 11:12:33 +0200

    useCallback-01 complete

--- react-exercises/src/useCounter.js
+++ react-exercises/src/useCounter.js
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 export function useCounter(initialValue = 0) {
   const [counter, setCounter] = useState(initialValue);
 
-  function handleCounterIncrement() {
+  const handleCounterIncrement = useCallback(function handleCounterIncrement() {
     setCounter((c) => c + 1);
-  }
+  }, []);
 
-  function handleCounterDecrement() {
+  const handleCounterDecrement = useCallback(function handleCounterDecrement() {
     setCounter((c) => c - 1);
-  }
+  }, []);
 
-  function handleCounterReset() {
+  const handleCounterReset = useCallback(function handleCounterReset() {
     setCounter(initialValue);
-  }
+  }, [initialValue]);
 
   return {
     counter: counter,
@@ -21,4 +21,23 @@ export function useCounter(initialValue = 0) {
     onDecrement: handleCounterDecrement,
     onReset: handleCounterReset,
   };
+
+  // function handleCounterIncrement() {
+  //   setCounter((c) => c + 1);
+  // }
+
+  // function handleCounterDecrement() {
+  //   setCounter((c) => c - 1);
+  // }
+
+  // function handleCounterReset() {
+  //   setCounter(initialValue);
+  // }
+
+  // return {
+  //   counter: counter,
+  //   onIncrement: handleCounterIncrement,
+  //   onDecrement: handleCounterDecrement,
+  //   onReset: handleCounterReset,
+  // };
 }
