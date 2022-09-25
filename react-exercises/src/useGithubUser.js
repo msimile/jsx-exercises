da268e128a67e645f77e5497140aa3f2ffdb5ce9
Marco Simile <marco.simile@outlook.it>
2022-09-25 20:08:08 +0200

    swr-01 complete

--- react-exercises/src/useGithubUser.js
+++ react-exercises/src/useGithubUser.js
@@ -1,34 +1,48 @@
 import { useState, useEffect } from "react";
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export function useGithubUser(username) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  async function fetchGithubUser(username) {
-    setLoading(true);
-    setError(null);
-
-    try {
-      const response = await fetch(`https://api.github.com/users/${username}`);
-      const json = await response.json();
-
-      setData(json);
-    } catch (error) {
-      setError(error);
-      setData(null);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    fetchGithubUser(username);
-  }, [username]);
-
+  const { data, error } = useSWR(
+    `https://api.github.com/users/${username}`,
+    fetcher
+  );
   return {
     data,
-    loading,
+    loading: !data && !error,
     error,
   };
 }
+// export function useGithubUser(username) {
+//   const [data, setData] = useState(null);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(null);
+
+//   async function fetchGithubUser(username) {
+//     setLoading(true);
+//     setError(null);
+
+//     try {
+//       const response = await fetch(`https://api.github.com/users/${username}`);
+//       const json = await response.json();
+
+//       setData(json);
+//     } catch (error) {
+//       setError(error);
+//       setData(null);
+//     } finally {
+//       setLoading(false);
+//     }
+//   }
+
+//   useEffect(() => {
+//     fetchGithubUser(username);
+//   }, [username]);
+
+//   return {
+//     data,
+//     loading,
+//     error,
+//   };
+// }
